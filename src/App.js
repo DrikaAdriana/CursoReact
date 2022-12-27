ee22c2ed5cbf938fc9be62ca6e58f8ff34bd6468
Adriana-Drika <nmdrika@gmail.com>
2022-12-27 19:13:42 -0300

    Curso React Aula 15

--- src/App.js
+++ src/App.js
@@ -2,23 +2,30 @@ import React,{useState} from 'react'
 
 export default function App() {
   
-  const [nome,setNome]=useState('')
-  const [carro,setCarro]=useState('HRV')
+  const [form,setForm]=useState({"nome":"", "curso":"", "ano":""})//UM STATE só para todos os elementos do formulário
+  const handlFormChange=(e)=>{//função manipuladora 
+    if(e.target.getAttribute('name')=='fnome'){
+      setForm({"nome":e.target.value,"curso":form.curso,"ano":form.ano})
+    }else if(e.target.getAttribute('name')=='fcurso'){
+      setForm({"nome":form.nome,"curso":e.target.value,"ano":form.ano})
+    }else if(e.target.getAttribute('name')=='fano'){
+      setForm({"name":form.name, "curso":form.curso, "ano":e.target.value})
+    }
+  }
 
   return(
     <>
-      <label>Digite seu nome</label>
-      <input type='text' name='fnome' value={nome} onChange={(e)=>setNome(e.target.value)}></input>
-      <p>Nome digitado no Input: {nome}</p>
+      <label>Nome</label>
+      <input type='text' name='fnome' value={form.nome} onChange={(e)=>handlFormChange(e)}/><br/>
+      <label>Curso</label>
+      <input type='text' name='fcurso' value={form.curso} onChange={(e)=>handlFormChange(e)}/><br/>
+      <label>Ano</label>
+      <input type='text' name='fano' value={form.ano} onChange={(e)=>handlFormChange(e)}/><br/>
 
-      <label>Selecione um carro</label>
-      <select value={carro} onChange={(e)=>setCarro(e.target.value)}>
-        <option value="HRV">HRV</option>
-        <option value="Golf">Golf</option>
-        <option value="Cruze">Cruze</option>
-        <option value="Argo">Argo</option>
-      </select>
-      <p>Cor selecionada: {carro}</p>
+
+      <p>Nome digitado: {form.nome}</p>
+      <p>Curso digitado: {form.curso}</p>
+      <p>Ano digitado: {form.ano}</p>
     </>
   );
 }
