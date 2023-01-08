be7b6d75dfb43cf449604642c3e3625cf7b4a4dd
Adriana-Drika <nmdrika@gmail.com>
2023-01-07 22:35:16 -0300

    corrigindo bug na renderização

--- src/index.js
+++ src/index.js
@@ -4,16 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-function tick(){
+
   ReactDOM.render(
     <React.StrictMode>
       <App />
     </React.StrictMode>,
     document.getElementById('root')
   );
-}
-setInterval(tick,1000)
-tick()
+
 
 
 
